7fbf0abea8e0fd8e68b2f04bce19bc760e27e624
yegorch <yegor.rb@gmail.com>
2021-04-27 15:43:36 +0600

    change errors and alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user.avatar?
       user.avatar.url
     else
-      asset_path('user.png')
+      asset_pack_path('media/images/user.png')
     end
   end
 
@@ -11,7 +11,7 @@ module ApplicationHelper
     if user.avatar.file.present?
       user.avatar.thumb.url
     else
-      asset_path('user.png')
+      asset_pack_path('media/images/user.png')
     end
   end
 
@@ -21,7 +21,7 @@ module ApplicationHelper
     if photos.any?
       photos.sample.photo.url
     else
-      asset_path('event.jpg')
+      asset_pack_path('media/images/event.jpg')
     end
   end
 
