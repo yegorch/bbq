8c9c2e5af8ae6584104967e8de24dc91a8520c43
yegorch <yegor.rb@gmail.com>
2021-05-02 14:15:19 +0600

    photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,9 +2,6 @@ class Photo < ApplicationRecord
   belongs_to :event
   belongs_to :user
 
-  validates :event, presence: true
-  validates :user, presence: true
-
   mount_uploader :photo, PhotoUploader
 
   scope :persisted, -> { where "id IS NOT NULL"}
