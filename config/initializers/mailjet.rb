7af02854b774ab108c44240b19abe39974827342
yegorch <yegor.rb@gmail.com>
2021-05-12 20:51:57 +0600

    mailjet

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -5,4 +5,11 @@ if Rails.env.development?
     config.default_from = ENV["MAILJET_DEV_SENDER"]
     config.api_version = 'v3.1'
   end
+elsif Rails.env.production?
+  Mailjet.configure do |config|
+    config.api_key = ENV["MAILJET_API_KEY"]
+    config.secret_key = ENV["MAILJET_SECRET_KEY"]
+    config.default_from = ENV["MAILJET_PROD_SENDER"]
+    config.api_version = 'v3.1'
+  end
 end
