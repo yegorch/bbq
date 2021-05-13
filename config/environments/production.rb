c3e9dcc0b3806d9d296caa3ba899c1d7c56f267b
yegorch <yegor.rb@gmail.com>
2021-05-13 15:54:18 +0600

    require 'action_mailer'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'appbbq.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :mailjet
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     address:              'in-v3.mailjet.com',
