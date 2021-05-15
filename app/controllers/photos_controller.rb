5206980d943d8b148ab42c06644ee2fe6224d97f
yegorch <yegor.rb@gmail.com>
2021-05-15 13:52:44 +0600

    valid photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,7 +8,9 @@ class PhotosController < ApplicationController
 
     @new_photo.user = current_user
 
-    if @new_photo.save
+    if @new_photo.valid? 
+      redirect_to root_path, alert: I18n.t('controllers.photos.error') 
+      @new_photo.save
       notify_subscribers(@event, @new_photo)
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
