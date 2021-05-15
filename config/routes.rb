1a2691db668ad82a433c0cb60c243306496f00b5
yegorch <yegor.rb@gmail.com>
2021-05-15 13:24:40 +0600

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -6,15 +6,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "events#index"
 
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
   resources :events do
     resources :comments, only: [:create, :destroy]
     resources :subscriptions, only: [:create, :destroy]
     resources :photos, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
-
 end
