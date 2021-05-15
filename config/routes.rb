881d72791af1083a259ef5c4c267d940a3f0f991
yegorch <yegor.rb@gmail.com>
2021-05-15 14:18:13 +0600

    fix resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-
-  resources :photos
-  resources :subscriptions
-  resources :comments
   devise_for :users
   root "events#index"
 
@@ -10,7 +6,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     resources :subscriptions, only: [:create, :destroy]
     resources :photos, only: [:create, :destroy]
-    post :show, on: :member
   end
   resources :users, only: [:show, :edit, :update]
 end
