1f256f0e8a7d6b0ca3f2b8400bf1bf7a47fc5ee9
yegorch <yegor.rb@gmail.com>
2021-05-30 19:56:44 +0600

    secrets yml

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 Mailjet.configure do |config|
-  config.api_key = ENV['MAILJET_API_KEY']
-  config.secret_key = ENV['MAILJET_SECRET_KEY']
-  config.default_from = ENV['MAILJET_SENDER']
+  config.api_key = Rails.application.secrets[:api_key]
+  config.secret_key = Rails.application.secrets[:secret_key]
+  config.default_from = Rails.application.secrets[:sender]
   config.api_version = 'v3.1'
 end
