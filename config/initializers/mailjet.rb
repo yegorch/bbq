4e2a4ed1f98369222795e49e31f84c3ea66dac9d
yegorch <yegor.rb@gmail.com>
2021-05-31 19:14:24 +0600

    mailer

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 Mailjet.configure do |config|
   config.api_key = Rails.application.credentials.mailjet[:api_key]
-  config.secret_key = Rails.application.seccredentials.mailjetrets[:secret_key]
+  config.secret_key = Rails.application.credentials.mailjet[:secret_key]
   config.default_from = Rails.application.credentials.mailjet[:sender]
   config.api_version = 'v3.1'
 end
